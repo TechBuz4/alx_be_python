1b77149aac590b53482b838cace90443665580e6
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2024-12-11 20:33:14 -0600

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -10,5 +10,5 @@ rate = 0.05
 # Project Annual Savings
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
-print(f"Your monthly savings are ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}.")
