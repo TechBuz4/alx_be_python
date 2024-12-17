6511a3bcfc50361e73ea92ccae070b67d7ea8ce8
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2024-12-16 21:40:41 -0600

    Update daily_reminder.py

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,23 +4,23 @@ The program will then provide a customized reminder for that task, demonstrating
 and loops without relying on data structures to store multiple tasks."""
 
 #prompting user for inputs
-task = input("Enter the task:")
+task = input("Enter your task:")
 priority = input("Priority (high/medium/low):")
-time_sensitive = input("Is it time-bound? (yes/no):").lower()
+time_bound = input("Is it time-bound? (yes/no):").lower()
 
 match priority:
     case "high":
-        if time_sensitive == "yes":
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
             print(f"Reminder: '{task}' is a {priority} priority task. Consider completing it asap")
     case "medium":
-        if time_sensitive == "yes":
+        if time_bound == "yes":
             print(f"Reminder: '{task}' is a {priority} priority task that requires immediate attention today!")
         else:
             print(f"Reminder: {task} is a {priority} priority task. Consider completing it soon")
     case "low":
-        if time_sensitive == "yes":
+        if time_bound == "yes":
             print(f"Reminder: {task} is a {priority} priority task that requires immediate attention today!")
         else:
             print(f"Reminder: {task} is a {priority} priority task. Consider completing it when you have free time.")
