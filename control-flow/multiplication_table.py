4b267ff9dfaaf44ce315c4b6385be47401da7bbf
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2024-12-16 21:21:36 -0600

    Create multiplication_table.py

---
+++ control-flow/multiplication_table.py
@@ -0,0 +1,9 @@
+#This script will ask the user to enter a number, then use a for loop to print the 
+# multiplication table for that number from 1 to 10.
+
+#prompting user for input
+num = int(input("Enter a number to see its multiplication table:"))
+
+for i in range(1, 11):
+    result = num * i
+    print(f"{num} * {i} = {result}")
