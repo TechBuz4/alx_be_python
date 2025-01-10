69ea1f792d46f531f00baddf82a0ca52ec3e129c
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-01-09 21:54:05 -0600

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,1 +1,14 @@
-fns_and_dsa
+# a calculator that performs arthimetic operations
+def perform_operation(num1, num2, operation):
+    if operation.lower() == 'add':
+        return num1 + num2
+    elif operation.lower() == 'subtract':
+        return num1 - num2
+    elif operation.lower() == 'multiply':
+        return num1 * num2
+    elif operation.lower() == 'divide':
+        if num2 == 0:
+            result = "Cannot divide by zero"
+            return result
+        else:
+            return num1 / num2
