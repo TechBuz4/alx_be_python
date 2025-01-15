4fc6b1be67becb448981132602da84a2fe6b729c
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-01-14 23:14:17 -0600

    Update simple_calculator.py

--- programming_paradigm/simple_calculator.py
+++ programming_paradigm/simple_calculator.py
@@ -1,5 +1,5 @@
 # simple_calculator.py
-class SimpleCalculator:
+class SimpleCalculator():
     """A simple calculator class that supports basic arithmetic operations."""
 
     def add(self, a, b):
