bf398c330dcb2b035f8dd25af563f3463a5c6e09
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-01-14 22:33:10 -0600

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -6,7 +6,8 @@ class BankAccount():
     def __init__(self, account_balance = 0):
         self.account_balance = account_balance
     def deposit(self, amount):
-        self.account_balance += amount
+        if amount > 0:
+            self.account_balance += amount
         return self.account_balance
     def withdraw(self, amount):
         if amount > self.account:
@@ -15,4 +16,4 @@ class BankAccount():
             self.account_balance -= amount
             return True
     def display_balance(self):
-        print(f"Your current balance is {self.account_balance}")
+        print(f"Your current balance is {self.account_balance:.2f}")
