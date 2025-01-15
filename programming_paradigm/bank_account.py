ff6b86aa0f954012b407cb10102dfe139aac7e42
Busayo Ososanwo <98640516+TechBuz4@users.noreply.github.com>
2025-01-14 22:22:54 -0600

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,1 +1,18 @@
+#Understand the fundamentals of OOP in Python by implementing a BankAccount class
+# that encapsulates banking operations. Use command line arguments to interact 
+# with instances of this class.
 
+class BankAccount():
+    def __init__(self, account_balance = 0):
+        self.account_balance = account_balance
+    def deposit(self, amount):
+        self.account_balance += amount
+        return self.account_balance
+    def withdraw(self, amount):
+        if amount > self.account:
+            return False
+        else:
+            self.account_balance -= amount
+            return True
+    def display_balance(self):
+        print(f"Your current balance is {self.account_balance}")
